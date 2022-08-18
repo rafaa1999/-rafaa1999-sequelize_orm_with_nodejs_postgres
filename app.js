9368367ff0f7f10d1984c5d46e0077123c9f785b
rafaa1999 <raferhouma10@gmail.com>
2022-08-18 23:47:24 +0100

    insert values to user's table using insomnia

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-const {sequelize} =require('./models')
+const express=require('express')
+const {sequelize,User} =require('./models')
 
-async function main(){
-    await sequelize.sync({force:true})
-}
+const app=express()
 
-main()
+app.use(express.json())
+
+
+app.post( '/users', async(req,res) => {
+
+    const{name,email,role}=req.body
+
+    try {
+
+        const user=await User.create({name,email,role})
+        return res.json(user)
+
+    } catch (error) {
+
+        console.log(error)
+        return res.status(500).json(error)
+        
+    }
+})
+
+
+
+app.listen({port:5000},async()=>{
+
+    console.log("server is runnning on hhht://localhost:5000")
+     await sequelize.sync({force:true})
+     console.log("database synced !")
+})
+
--- models/user.js
+++ models/user.js
@@ -14,6 +14,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
+    uuid:{
+      type:DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV4
+
+    },
     name:{
       type:DataTypes.STRING,
       allowNull:false
